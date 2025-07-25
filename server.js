4181f68f0ce4e144ee7c6d99d945b30fd99599dc
leniejoice <lr.ljmendoza@mmdc.mcl.edu.ph>
2025-07-25 22:53:20 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
 
 db.connect((err) => {
   if (err) {
